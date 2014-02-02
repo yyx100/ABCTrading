b3460f128425759e19d39734d3401f33a869702c
yyx100 <yyx100@gmail.com>
2014-02-01 19:39:55 -0500

    add age to person

--- src/main/java/org/gradle/Person.java
+++ src/main/java/org/gradle/Person.java
@@ -5,8 +5,18 @@ import org.apache.commons.collections.list.GrowthList;
 public class Person {
     private final String name;
     private final String sex = null; 
+    private int age = 0;
+    
 
-    public Person(String name) {
+    public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Person(String name) {
         this.name = name;
         new GrowthList();
     }
